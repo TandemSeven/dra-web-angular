f7cf4652a83ca801d6ae50cbcac310a0e07f05e0
djennings <djennings@tandemseven.com>
2018-11-06 09:09:33 -0500

    Add flex and ngrx, break out material modules and block off main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent,
+    WeatherForecastComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
